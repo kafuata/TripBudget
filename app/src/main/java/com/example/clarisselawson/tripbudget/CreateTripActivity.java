9008945d2188b26a92ecbbf4ac6c0365943d31a6
Clarisse LAWSON <clarisse.lawson@hetic.net>
2016-06-10 23:37:38 +0200

    use Google PlaceAutoComplete in CreateTripActivity

--- app/src/main/java/com/example/clarisselawson/tripbudget/CreateTripActivity.java
+++ app/src/main/java/com/example/clarisselawson/tripbudget/CreateTripActivity.java
@@ -3,16 +3,15 @@ package com.example.clarisselawson.tripbudget;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.InputType;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
 import android.widget.AutoCompleteTextView;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -20,17 +19,38 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.clarisselawson.tripbudget.Database.TripDBHelper;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.places.AutocompletePrediction;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 
-
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
-import static java.util.Locale.FRANCE;
+public class CreateTripActivity extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener{
 
-public class CreateTripActivity extends AppCompatActivity implements View.OnClickListener {
+    private static String TAG = "CreateTripActivity";
+    /**
+     * GoogleApiClient wraps our service connection to Google Play Services and provides access
+     * to the user's sign in state as well as the Google's APIs.
+     */
+    protected GoogleApiClient mGoogleApiClient;
+    private PlaceAutocompleteAdapter mAdapter;
+    private AutoCompleteTextView mAutocompleteView;
+
+    // http://stackoverflow.com/a/13824556
+    private static final LatLngBounds EARTH = new LatLngBounds(
+            new LatLng(-84.9, -180), // top left corner of map
+            new LatLng(84.9, 180) // bottom right corner
+            );
 
     private TripDBHelper myDb;
     private String DATABASE_NAME = "tripDB";
@@ -52,12 +72,6 @@ public class CreateTripActivity extends AppCompatActivity implements View.OnClic
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_trip);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_dropdown_item_1line, getCountries());
-        AutoCompleteTextView textView = (AutoCompleteTextView)
-                findViewById(R.id.trip_destination);
-        textView.setAdapter(adapter);
 
         dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.FRANCE);
 
@@ -85,7 +99,33 @@ public class CreateTripActivity extends AppCompatActivity implements View.OnClic
             }
         });
 
+        // Construct a GoogleApiClient for the {@link Places#GEO_DATA_API} using AutoManage
+        // functionality, which automatically sets up the API client to handle Activity lifecycle
+        // events. If your activity does not extend FragmentActivity, make sure to call connect()
+        // and disconnect() explicitly.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, 0 /* clientId */, this)
+                .addApi(Places.GEO_DATA_API)
+                .build();
 
+        /*
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, getCountries());
+        AutoCompleteTextView textView = (AutoCompleteTextView)
+                findViewById(R.id.trip_destination);
+        textView.setAdapter(adapter);
+        */
+        // Retrieve the AutoCompleteTextView that will display Place suggestions.
+        mAutocompleteView = (AutoCompleteTextView)
+                findViewById(R.id.trip_destination);
+
+        // Register a listener that receives callbacks when a suggestion has been selected
+        mAutocompleteView.setOnItemClickListener(mAutocompleteClickListener);
+
+        // Set up the adapter that will retrieve suggestions from the Places Geo Data API that cover
+        // the entire world.
+        mAdapter = new PlaceAutocompleteAdapter(this, mGoogleApiClient, EARTH, null);
+        mAutocompleteView.setAdapter(mAdapter);
     }
 
 
@@ -166,9 +206,86 @@ public class CreateTripActivity extends AppCompatActivity implements View.OnClic
         startActivity(intent);
     }
 
+    /*
     private static String[] getCountries() {
         return new String[]
                 {"Belgium", "France", "Italy", "Germany", "Spain"};
     }
+    */
 
+    /**
+     * Listener that handles selections from suggestions from the AutoCompleteTextView that
+     * displays Place suggestions.
+     * Gets the place id of the selected item and issues a request to the Places Geo Data API
+     * to retrieve more details about the place.
+     *
+     * @see com.google.android.gms.location.places.GeoDataApi#getPlaceById(com.google.android.gms.common.api.GoogleApiClient,
+     * String...)
+     */
+    private AdapterView.OnItemClickListener mAutocompleteClickListener
+            = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            /*
+             Retrieve the place ID of the selected item from the Adapter.
+             The adapter stores each Place suggestion in a AutocompletePrediction from which we
+             read the place ID and title.
+              */
+            final AutocompletePrediction item = mAdapter.getItem(position);
+            final String placeId = item.getPlaceId();
+            final CharSequence primaryText = item.getPrimaryText(null);
+
+            Log.i(TAG, "Autocomplete item selected: " + primaryText);
+
+            /*
+             Issue a request to the Places Geo Data API to retrieve a Place object with additional
+             details about the place.
+              */
+            PendingResult<PlaceBuffer> placeResult = Places.GeoDataApi
+                    .getPlaceById(mGoogleApiClient, placeId);
+            placeResult.setResultCallback(mUpdatePlaceDetailsCallback);
+
+            Toast.makeText(getApplicationContext(), "Clicked: " + primaryText,
+                    Toast.LENGTH_SHORT).show();
+            Log.i(TAG, "Called getPlaceById to get Place details for " + placeId);
+        }
+    };
+
+    /**
+     * Callback for results from a Places Geo Data API query that shows the first place result in
+     * the details view on screen.
+     */
+    private ResultCallback<PlaceBuffer> mUpdatePlaceDetailsCallback
+            = new ResultCallback<PlaceBuffer>() {
+        @Override
+        public void onResult(PlaceBuffer places) {
+            if (!places.getStatus().isSuccess()) {
+                // Request did not complete successfully
+                Log.e(TAG, "Place query did not complete. Error: " + places.getStatus().toString());
+                places.release();
+                return;
+            }
+            // Get the Place object from the buffer.
+            final Place place = places.get(0);
+
+            Toast.makeText(getApplicationContext(), place.getName(), Toast.LENGTH_SHORT).show();
+
+            // Format details of the place for display and show it in a TextView.
+            /*
+            place.getName(),
+            place.getId(),
+            place.getAddress(),
+            place.getPhoneNumber(),
+            place.getWebsiteUri();
+             */
+
+            Log.i(TAG, "Place details received: " + place.getName());
+            places.release();
+        }
+    };
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
